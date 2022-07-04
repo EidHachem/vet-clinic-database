5748ad50fee4c62cd57fd3d68ba7b9c1acaafb68
EidHachem <eidhachem1@gmail.com>
2022-07-04 17:28:47 +0300

    Add animals table and answer project queries

--- queries.sql
+++ queries.sql
@@ -1,3 +1,10 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals WHERE name = 'Luna';
+SELECT * from animals WHERE name LIKE '%mon';
+SELECT name from animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
+SELECT name from animals WHERE neutered AND escape_attempts < 3; 
+SELECT date_of_birth FROM animals WHERE name IN ('Agumon', 'Pikachu');
+SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
+SELECT * from animals WHERE neutered;
+SELECT * FROM animals WHERE name != 'Gabumon';
+SELECT * FROM animals WHERE weight_kg BETWEEN '10.4' AND '17.3';
