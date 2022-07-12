95e36edfc09e9180bf4c724f7aa2de7e4744b179
Manel <manelhammouche3@gmail.com>
2022-07-12 13:42:58 +0100

    Add tables

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -20,3 +20,23 @@ CREATE TABLE invoices(
   paid_at TIMESTAMP,
   medical_history_id INT REFERENCES medical_histories(id)
 );
+
+CREATE TABLE treatments(
+  id SERIAL PRIMARY KEY,
+  type VARCHAR(50),
+  name VARCHAR(100)
+);
+
+CREATE TABLE treatment_histories(
+  treatment_id INT REFERENCES treatments(id),
+  medical_history_id INT REFERENCES medical_histories(id)
+);
+
+CREATE TABLE invoice_items(
+  id SERIAL PRIMARY KEY,
+  unit_price DECIMAL(3, 2),
+  quantity INT,
+  total_price DECIMAL(3, 2),
+  invoice_id INT REFERENCES invoices(id),
+  treatment_id INT REFERENCES treatments(id)
+);
